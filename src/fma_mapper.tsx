ee2e2d0cf31e0d46584f292d948c584195333107
Bryce Mercines <nplixel@gmail.com>
2020-11-11 00:06:34 +0800

    completed implementation of first FMA Mapping functions, from geojson dataset

--- src/fma_mapper.tsx
+++ src/fma_mapper.tsx
@@ -2,21 +2,27 @@ const d3geo = require("d3-geo");
 const fma_data = require("../fma.json");
 const GeoJsonGeometriesLookup = require("geojson-geometries-lookup");
 
-console.log(fma_data);
-
 const glookup = new GeoJsonGeometriesLookup(fma_data);
 
-const getFMA = (coordinates) => {
+// Determine if user in under Fishery Manage Area GeoJSON Data
+// coordinates format: [lat, log]
+const isFMA = (coordinates) => {
   return d3geo.geoContains(fma_data, coordinates);
 };
 
-let coords = [120.783102, 14.581689];
+let coords = [118.500998, 10.512506];
 
-let out = getFMA(coords);
-console.log(out);
+// Look up which FMA a certain gps coodinate belongs to.
+// coordinates format: [lat, log]
+const getFMA = (coordinates) => {
+  let targetCoordinates = { type: "Point", coordinates: coordinates };
+  let result = glookup.getContainers(targetCoordinates, { ignorePoints: true });
 
-const point1 = { type: "Point", coordinates: coords };
+  if (result.features.length) {
+    return result.features[0].properties.FMA;
+  } else {
+    return false;
+  }
+};
 
-let lu = glookup.getContainers(point1, { ignorePoints: true });
-
-console.log(lu.features[0].properties);
+console.log(getFMA(coords));
