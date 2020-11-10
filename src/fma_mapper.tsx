722cb01d0821e316eef6fecec99a0232a00db295
Bryce Mercines <nplixel@gmail.com>
2020-11-10 10:34:33 +0800

    successful coordinate validator function for checking if data originated from a valid coordinate and can be automatically categorized

--- src/fma_mapper.tsx
+++ src/fma_mapper.tsx
@@ -1,13 +1,22 @@
 const d3geo = require("d3-geo");
 const fma_data = require("../fma.json");
+const GeoJsonGeometriesLookup = require("geojson-geometries-lookup");
 
 console.log(fma_data);
+
+const glookup = new GeoJsonGeometriesLookup(fma_data);
 
 const getFMA = (coordinates) => {
   return d3geo.geoContains(fma_data, coordinates);
 };
 
-let coords = [-91.33057280941561, 38.14850647789078];
+let coords = [120.783102, 14.581689];
 
 let out = getFMA(coords);
 console.log(out);
+
+const point1 = { type: "Point", coordinates: coords };
+
+let lu = glookup.getContainers(point1, { ignorePoints: true });
+
+console.log(lu.features[0].properties);
