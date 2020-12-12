9c1f3b89d95fda7c69ec0e6e12ee904fa205755c
Bryce Mercines <nplixel@gmail.com>
2020-12-12 15:46:51 +0800

    added endpoint for analytics

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -8,6 +8,7 @@ const fma_list = fmaData.map((x:any)=> x.fma)
 
 let incident_summary = [];
 let incidents_data = lowdb.get("incidents").value();
+let incidentsByDate = []
 
 let incidents_normalized_dates = incidents_data.map((x:any)=>{
   let incident = x;
@@ -15,6 +16,31 @@ let incidents_normalized_dates = incidents_data.map((x:any)=>{
   return incident
 })
 
+// prepare incident overview data
+fma_list.forEach((fma:any)=>{
+  let date_groups = []
+  let filtered_by_fma = incidents_normalized_dates.filter((f:any)=> f.fma === fma)
+
+  filtered_by_fma.forEach((s:any)=>{
+    let dateInstance = date_groups.find((ic:any)=> ic.date === s.date_reported)
+    if(!dateInstance){
+      date_groups.push({
+        date: s.date_reported,
+        records: [s]
+      })
+    }else{
+      dateInstance.records.push(s)
+    }
+  })
+
+  let fma_record = {
+   fma: fma,
+   records: date_groups
+ }
+ incidentsByDate.push(fma_record)
+})
+
+// prepare complete incidents grouped by date
 fma_list.forEach((fma:any)=>{
   let date_groups = []
   let filtered_by_fma = incidents_normalized_dates.filter((f:any)=> f.fma === fma)
@@ -48,10 +74,16 @@ AnalyticsRoute.get("/", (req, res) => {
   res.send(response)
 });
 
-AnalyticsRoute.get("/overview", (req, res) => {
+AnalyticsRoute.get("/incidents/overview", (req, res) => {
   let response = incident_summary
   res.send(response)
 });
+
+AnalyticsRoute.get("/incidents/complete", (req, res) => {
+  let response = incidentsByDate
+  res.send(response)
+});
+
 
 
 module.exports = AnalyticsRoute;
